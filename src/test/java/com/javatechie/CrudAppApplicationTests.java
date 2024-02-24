bf85c9f1b3b84ad4cc3a1c9d12fc7cf126e1d730
javatechie <basanta.kumar.hota.4@gmail.com>
2024-02-24 10:17:08 +0530

    test case added

--- src/test/java/com/javatechie/CrudAppApplicationTests.java
+++ src/test/java/com/javatechie/CrudAppApplicationTests.java
@@ -1,13 +1,22 @@
 package com.javatechie;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
+@Slf4j
 class CrudAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        //sample test
+        int i = 10;
+        int j = 10;
+        log.info("Test method executed with value i {} and j {}", i, j);
+        assertEquals(i, j);
+    }
 
 }
